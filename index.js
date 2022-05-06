ea24b7b03092efbc44011bb90fad8a189820aff6
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-05-06 22:36:35 +0600

    server succesfully running

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("running perfume server");
+  res.send("hey iam running");
 });
 
 app.listen(port, () => {
