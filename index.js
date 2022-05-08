c10d9996bfeb52f0590ceb9e0ace1306f05530bc
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-05-08 16:17:54 +0600

    single item api set

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -26,6 +26,13 @@ async function run() {
       const cursor = perfumeCollection.find(query);
       const perfumes = await cursor.toArray();
       res.send(perfumes);
+    });
+
+    app.get("/perfume/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const perfume = await perfumeCollection.findOne(query);
+      res.send(perfume);
     });
   } finally {
   }
