da5b557c72d2a11cf5cd749cb28c95762aac9d01
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-05-09 22:09:55 +0600

    heroku deploying

--- index.js
+++ index.js
@@ -78,6 +78,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
 app.listen(port, () => {
   console.log("listening", port);
 });
