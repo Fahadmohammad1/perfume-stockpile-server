2029c9d1e5d8afdd7550e6ec2d3cb2fa0eb656f2
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-05-09 20:03:10 +0600

    filter by user api added

--- index.js
+++ index.js
@@ -64,6 +64,15 @@ async function run() {
       const result = await perfumeCollection.insertOne(newItem);
       res.send(result);
     });
+
+    app.get("/myItems", async (req, res) => {
+      const email = req.query.email;
+
+      const query = { email: email };
+      const cursor = perfumeCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
   } finally {
   }
 }
